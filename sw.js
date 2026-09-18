f76eb2b56b144c71c79b0034e218f1e83550e32b
DerTakku <133666284+DerTakku@users.noreply.github.com>
2026-09-18 12:17:26 +0200

    Align shotlist with wedding schedule

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'wedding-shotlist-v1';
+const CACHE = 'wedding-shotlist-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => { if (event.request.method === 'GET') event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
