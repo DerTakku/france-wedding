1ccaf9fe56accd8a8a277ff3b95c7e51244caffe
DerTakku <133666284+DerTakku@users.noreply.github.com>
2026-09-18 12:11:13 +0200

    Add offline wedding shotlist PWA

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'wedding-shotlist-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => { if (event.request.method === 'GET') event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
